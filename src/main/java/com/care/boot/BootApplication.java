af6d9f191ba5c49263627244c2b08a8e4ac2c5c6
SUS7898 <dntmd7898@naver.com>
2026-04-29 14:13:05 +0900

    외부 tomcat 설정 테스트

--- src/main/java/com/care/boot/BootApplication.java
+++ src/main/java/com/care/boot/BootApplication.java
@@ -3,13 +3,19 @@ package com.care.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder; // 추가됨
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // 추가됨
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class BootApplication {
+public class BootApplication extends SpringBootServletInitializer { // 1. 상속 추가
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
-	}
+    // 2. 외장 톰캣이 이 앱을 시작할 수 있게 해주는 설정 추가
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
 }
