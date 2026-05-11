4f08068eeda12b8a70bbebd4d90bb2c7b8063184
Unoh03 <unoh0626@gmail.com>
2026-05-11 09:59:41 +0900

    캐시 업뎃 1

--- src/main/java/com/care/boot/BootApplication.java
+++ src/main/java/com/care/boot/BootApplication.java
@@ -3,18 +3,9 @@ package com.care.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class BootApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 여기에 에러 페이지 필터 중복 등록 방지 코드를 추가합니다.
-        setRegisterErrorPageFilter(false); 
-        return builder.sources(BootApplication.class);
-    }
+public class BootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
